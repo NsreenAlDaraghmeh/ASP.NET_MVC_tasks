b4ccdf95b77469099c876d982df458dc9d2e6e18
nsreen <nsreengmail.com>
2023-02-01 21:31:57 +0000

    task1-2

---
+++ WebApplication3/WebApplication3/Controllers/taskController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class taskController : Controller
+    {
+        // GET: task
+        public ActionResult task0()
+        {
+          
+            List<Models.student> students = new List<Models.student>();
+            students.Add(new Models.student() { Id = 1, Name = "Nsreen", Major = "Mathematics", Faculity = "Sciences and Literature", });
+            students.Add(new Models.student() { Id = 2, Name = "Laila", Major = "English", Faculity = "Sciences and Literature", });
+            students.Add(new Models.student() { Id = 3, Name = "Kamel", Major = "Mechanical Engineering", Faculity = "Engineering", });
+            return View(students);
+        }
+    }
+}
---
+++ WebApplication3/WebApplication3/Models/student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Major { get; set; }
+        public string Faculity { get; set; }
+
+    }
+}
