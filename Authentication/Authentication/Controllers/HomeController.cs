446935d534b1afadd3df38bb8458dbe18412c9cd
nsreen <nsreengmail.com>
2023-02-09 16:41:11 +0000

    Task8-2

---
+++ Authentication/Authentication/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Authentication.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Authentication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private templateEntities1 db = new templateEntities1();
+        public ActionResult Index()
+        {
+            return View(db.Categories.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Categories()
+        {
+            ViewBag.Message = "Your Ctegories page.";
+
+            return View(db.Categories.ToList());
+        }
+
+      
+
+    
+
+
+      
+
+    }
+}
